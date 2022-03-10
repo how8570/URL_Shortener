5b2ed53de5eb982fab548a0e4a458946f3611e6e
how8570 <joe13512345@gmail.com>
2022-03-10 14:31:34 +0800

    Add dependency and .gitignore file

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/how8570/URL_Shortener.git
+
+go 1.17
+
+require github.com/gorilla/mux v1.8.0
--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func HandleIndex(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "index")
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	router := mux.NewRouter()
+	router.HandleFunc("/", HandleIndex)
+	err := http.ListenAndServe(":80", router)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
